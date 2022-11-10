cdeddeaccf06dba7aed7fe9037f6aa1832ab6bde
Ricardo <quintana_rr@hotmail.com>
2022-11-09 21:37:48 -0300

    consumiendo api

--- src/components/Api/Api.jsx
+++ src/components/Api/Api.jsx
@@ -1,34 +1,9 @@
 import { useEffect, useState } from "react";
-import CardItem from "../Item/CardItem";
+/*import CardItem from "../Item/CardItem";*/
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function apiList () {
-    const [mercadoList, setmercadoList] = useState([]);
-    const [params, setparams]= useState({
-        offset: 0,
-        limit: 12
-    })
-
-    const buscarList = "bulldog";
-    const url = `https://api.mercadolibre.com/sites/MLA/search?q=bulldog`;
-
-    useEffect(()=>{
-        fetch (url)
-        .then ((respuesta)=>respuesta.json())
-        .then ((json)=> setmercadoList(json.results))
-        .catch((error)=> console.log(error))
-    }, [url, params]);
-
-    return (
-        <div>   
-                {mercadoList.map((bulldog) => (
-                    <CardItem
-                    key={bulldog.id}
-                    title={bulldog.title}
-                    url={bulldog.thumbnail}
-                    />
-                ))}
-        </div>
-    )
+function ApiList () {
 }
 
-export default apiList;
+export default ApiList;
--- src/components/Item/CardItem.jsx
+++ src/components/Item/CardItem.jsx
@@ -1,23 +1,51 @@
+import { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function CardItem ({ title, url}) {
+
+function CardItem () {
+  const [mercadoList, setmercadoList] = useState([]);
+  const [params, setparams] = useState({
+      offset: 0,
+      limit: 12
+  });
+
+
+const query = "bulldog";
+  const url = `https://api.mercadolibre.com/sites/MLA/search?q=${query}&limit=${params.limit}&offset=${params.offset}`;
+
+
+const apiFetch = async () => {
+  const response = await fetch (url);
+  const json = await response.json();
+  setmercadoList(json.results)
+  }
+
+useEffect( ()=> {
+  apiFetch();
+}, [])
+
   return (
-    <div>
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <img src={url} alt={title} />
-        <Card.Text>
-          Ingrese para conocer los detalles
-        </Card.Text>
-        <Button variant="primary">Ver mas</Button>
-      </Card.Body>
-    </Card>
-    </div>
-    
-    
-  );
+      <div className="row">   
+      <h1 style={{textAlign: "center"}}>MUNDO CAMINO</h1>
+              {mercadoList.map((item) => (
+                  <div className="col-md-3 my-3">
+                      <Card style={{ width: 'auto', height: "350px", background:"yellow", textAlign:"center",padding:"20px"}} >
+                      <Card.Body>
+                      <Card.Title>{item.title}</Card.Title>
+                      <img src={item.thumbnail} alt={item.title} style={{width:"auto"}}/>
+                      <Card.Text>${item.price}</Card.Text>
+                      <Card.Text>
+                          Ingrese para conocer los detalles
+                      </Card.Text>
+                      <Button variant="primary">Ver mas</Button>
+                  </Card.Body>
+                  </Card>
+                  </div>
+                  
+              ))}
+      </div>
+  )
 }
 
 export default CardItem;
